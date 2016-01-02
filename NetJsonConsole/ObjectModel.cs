569b0e081aa1a83d254f90a991a5dc754ddb6c98
James <dev.jpsmith@gmail.com>
2016-01-02 13:20:37 -0700

    Added ObjectModel

---
+++ NetJsonConsole/ObjectModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace NetJsonConsole
+{
+    /// <summary>
+    /// Simple class used to represent some real-world data for our 
+    ///     serializer to work with
+    /// </summary>
+    [DataContract]
+    public class ObjectModel
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public string Value { get; set; }
+        [DataMember]
+        public bool Deleted { get; set; }
+
+    }
+}
